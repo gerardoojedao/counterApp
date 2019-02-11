13a6e46d475259df55f97dac848d0f9918cdb135
Gerardo <gerardo.ojeda.o@gmail.com>
2019-02-11 19:43:21 -0300

    basic folder structure

---
+++ CounterApp/SupportFiles/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  CounterApp
+//
+//  Created by Gerardo Ojeda on 11-02-19.
+//  Copyright © 2019 Gerardo Ojeda. All rights reserved.
+//
+
+import Foundation
+
+
+enum Constants {
+    
+}
