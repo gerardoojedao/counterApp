de3ccfa60b98d618718edda2b2b090a0e2d9d04e
Gerardo <gerardo.ojeda.o@gmail.com>
2019-02-13 00:11:46 -0300

    switch to VIPER Architecture and refactor code

--- CounterApp/SupportFiles/AppDelegate.swift
+++ CounterApp/SupportFiles/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let listCountersController = ListCountersViewController()
+        let listCountersController = ListCounterRouter.createModule()
         let navigationController = UINavigationController(rootViewController: listCountersController)
         
         self.window?.rootViewController = navigationController
